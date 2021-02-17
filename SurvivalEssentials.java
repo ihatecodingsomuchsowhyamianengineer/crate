496aea5668634047ef5c070fed29068a86d9010f
ihatecodingsomuchsowhyamianengineer <78659323+ihatecodingsomuchsowhyamianengineer@users.noreply.github.com>
2021-02-17 06:47:01 -0800

    Update SurvivalEssentials.java

--- SurvivalEssentials.java
+++ SurvivalEssentials.java
@@ -9,7 +9,10 @@ public class SurvivalEssentials implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "anchor"), ANCHOR_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "anchor"), new BlockItem(ANCHOR_BLOCK, new Item.Settings().group(ItemGroup.DECORATIONS)));
+        //crate
+        Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, "crate"), BlockEntityType.Builder.create(CrateBlockEntity::new, CRATE_BLOCK).build(null));
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "crate"), CRATE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "crate"), new BlockItem(CRATE_BLOCK, new Item.Settings().group(ItemGroup.DECORATIONS)));
+
     }
   }
